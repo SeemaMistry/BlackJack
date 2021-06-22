f164a210002f179d436289ad0bc9ed211afda580
SeemaMistry <seema.m.1996@gmail.com>
2021-06-22 01:22:44 -0400

    Create Deck class that prints the deck, printDeck()

---
+++ deck.py
@@ -0,0 +1,34 @@
+# Deck Class:
+#   A deck of cards using nummerical values. Functions contain: showCard(), get_random_card(), cardCount(card)
+# Helper random Class: (Private)
+#   Random Number generator (1-13) for selecting cards
+
+# importing helper modules
+import random
+
+# make a deck of cards with associated functions
+class Deck:
+    def __init__(self):
+        self.deck = []
+        # add 4 cards per index in the array
+        for i in range(14):
+            self.deck.append(4)
+    
+    # showCard(card) return the card IF card exists in the deck as true. Else, if card is not in deck return false.
+    # wait ... might not need this function
+
+    # cardCount(card) return how many cards exists (0-4) in the deck.
+    # get_random_card() return random card using random class
+
+    # printDeck() print the deck array
+    def printDeck(self):
+        # for i in self.deck:
+        #     print (str(i) + " has " + str(self.deck[i])+ "\n")
+        i = 1
+        while i < len(self.deck):
+            print (str(i) + " has " + str(self.deck[i])+ " cards\n")
+            i+=1
+
+
+game = Deck()
+game.printDeck()
