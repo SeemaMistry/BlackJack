1ecbe91af59a7aa751422188b95389f8d56875a9
SeemaMistry <seema.m.1996@gmail.com>
2021-06-22 02:33:37 -0400

    Create People class and begin planning out class functions and Dealer child class with comments

---
+++ people.py
@@ -0,0 +1,16 @@
+# People class to create players and dealers for card games
+# Import Deck class
+
+class People:
+    # initialize variable 
+
+    # pointsCounter() shows total points accumulated by player
+    # addPoint() add a point for the player when they win
+    
+    # cardDealt() store cards player is holding in an array. Print it on console (players cards are shown up)
+    # hit() add a card to player hand
+    # ruleCheck() checks players cards to see if you won or bust
+
+
+class Dealer(People):
+    # same as the people class but need to modify the cardDealt() to only show cards in index 1+ of array, not index 0
