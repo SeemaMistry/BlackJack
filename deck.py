bda9c3ab26bb19936f673b1dc5c49d1170b5fa8e
SeemaMistry <seema.m.1996@gmail.com>
2021-06-22 01:46:46 -0400

    Create cardCount(card) and get_random_card() functions to get random card from the deck

--- deck.py
+++ deck.py
@@ -16,9 +16,28 @@ class Deck:
     
     # showCard(card) return the card IF card exists in the deck as true. Else, if card is not in deck return false.
     # wait ... might not need this function
+    # no do need this helper function for when using random num generator
 
     # cardCount(card) return how many cards exists (0-4) in the deck.
+    def cardCount(self, card):
+        # go to index in array and see array.value
+        cardCount = self.deck[card]
+        # return array value
+        return cardCount
+
     # get_random_card() return random card using random class
+    def get_random_card(self):
+        # store a random number from 1-13 into cardValue
+        cardValue = random.randint(1,13)
+        # find cardValue in array and check if cardCount > 0. If greater decrement value by one, else run again
+        cardExist = False
+        while cardExist == False:
+            if self.cardCount(cardValue) > 0:
+                cardExist = True
+                self.deck[cardValue] -= 1
+                return cardValue
+            else:
+                cardValue = random.randint(1,13)
 
     # printDeck() print the deck array to console
     def printDeck(self):
@@ -30,3 +49,10 @@ class Deck:
 
 game = Deck()
 game.printDeck()
+print("Checking how many 10 cards exist in the deck:")
+print(game.cardCount(10))
+
+print("\nUse random number generator to pick a card and then show it. Also check deck for decrement:")
+print(game.get_random_card())
+game.printDeck()
+
