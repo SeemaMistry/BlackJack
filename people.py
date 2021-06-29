e271e3463bc56c0dc91da3f35995b1fe9632bd28
SeemaMistry <seema.m.1996@gmail.com>
2021-06-29 19:08:29 -0400

    Been a while, needed to regroup. Work on Player Class (formally called People) step by step. Ignore it's relation to the Deck class and focus on getting the player's ability to accummulate points, hold values in an array (cards), get and set their name, and print the hand they have. Once it's working then move on to more complicated things

--- people.py
+++ people.py
@@ -2,14 +2,13 @@
 # Import Deck class
 from deck import Deck
 
-class People:
+class Player:
     # initialize the person's attributes: hand, points (initialize to 0), player_status (true for playing, false when not hitting anymore), player_name
     def __init__(self, name):
-        self.points = 0
-        self.player_status = True
-        self.points = 0
-        self.name = name
-        self.hand = []
+        self.player_status = True # if player wants to continue playing or show cards
+        self.points = 0 # win = 1 point, loss is no points
+        self.name = self.setName(name) # name the player
+        self.hand = [] # all the cards player has is stored in array
 
     # getPoints() shows total points accumulated by player
     def getPoints(self):
@@ -19,13 +18,37 @@ class People:
     def addPoint(self):
         # add a point only when player won. One win = One point
         self.point +=1
+
+    # get and set player's name
+    def getName(self):
+        return self.name
+    
+    def setName(self, name):
+        self.name = name
+
+    def printHand(self):
+        # # print the values separated by comma
+        # for i in self.hand:
+        #     print(str(i) + ", ")
+
+        # apparently you can print arrays by just printing the array name
+        print(self.hand)
+
+    # add a card to your hand
+    def addHand(self, card):
+        self.hand.append(card)
+        
     
     def getHand(self):
         return self.hand
     
-    def setHand(self, card):
-        # take the card and add it to the player's hand
-        self.hand.append(card)
+    # def setHand(self, card):
+    #     # take the card and add it to the player's hand
+    #     self.hand.append(card)
+    def setHand(self):
+        card = Deck().get_random_card()
+        self.hand.append(card.getRandomCard())
+        
     
     # printHand() Print playerHand() on console every turn (players cards are all shown up)
     def printHand(self):
@@ -33,7 +56,7 @@ class People:
         # Name: card1, card2, card3, ....
         print(str(self.name) + ": ")
         for i in self.hand:
-            print(i.printCard())
+            print(i)
 
     # playerCall(str) string check for hit, pass
     # while loop until player_status is false
@@ -49,10 +72,10 @@ class People:
     # printHand() only show 2nd card, first card should be hidden
     # printFullHand() show all the cards only at the end of the game
 
-p1 = People("Player1")
-p1.printHand()
-card = Deck()
-card.get_random_card()
-card.printCard()
-p1.setHand(card)
-p1.printHand()
+# p1 = People("Player1")
+# p1.printHand()
+# card = Deck()
+# card.get_random_card()
+# #card.printCard()
+# p1.setHand()
+# p1.printHand()
