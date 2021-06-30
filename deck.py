b2ee50b5cca1fe20cc42e6a884a6fcf2ebe0e3c8
SeemaMistry <seema.m.1996@gmail.com>
2021-06-29 21:15:10 -0400

    Rename person.py to player.py. Clear deck.py

--- deck.py
+++ deck.py
@@ -1,2 +1,16 @@
+class Deck:
+    def __init__(self):
+        self.cards = []
+        # initalize a built deck of 52 cards of 4 suits
+        #self.build() 
+
+    # def build(self):
+    #     # use double for loops to create Card objects of 4 suits and value
+    #     # currently for value just use 1-13
+    #     suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
+    #     for s in suits:
+    #         print(s)
+
+deck = Deck()
 
 
