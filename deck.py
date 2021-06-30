633b5b5a876b26382343034dc1015c08ee17ff6c
SeemaMistry <seema.m.1996@gmail.com>
2021-06-29 21:30:45 -0400

    Make Deck.build() initialize a new full deck upon initialization of Deck Object creation. Create print function for test

--- deck.py
+++ deck.py
@@ -1,16 +1,25 @@
+# import the Card class
+from card import Card
 class Deck:
     def __init__(self):
         self.cards = []
         # initalize a built deck of 52 cards of 4 suits
-        #self.build() 
+        self.build() 
 
-    # def build(self):
-    #     # use double for loops to create Card objects of 4 suits and value
-    #     # currently for value just use 1-13
-    #     suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
-    #     for s in suits:
-    #         print(s)
+    def build(self):
+        # use double for loops to create Card objects
+        # Make 4 ranks of each suit
+        suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
+        ranks = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+        for s in suits:
+            for r in ranks:
+                #print ("{} of {}".format(r,s))
+                self.cards.append(Card(s,r))
+
+    def printDeck(self):
+        # loop through and print all the cards from the deck (use Card.py's show() function)
+        for card in self.cards:
+            card.show()
 
 deck = Deck()
-
-
+deck.printDeck()
