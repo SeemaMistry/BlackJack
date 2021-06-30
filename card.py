abfb8665c9fba3155ee0c7c74672af7fa9567f5e
SeemaMistry <seema.m.1996@gmail.com>
2021-06-30 01:09:06 -0400

    Clean up Card class comments. No changes made to code

--- card.py
+++ card.py
@@ -1,8 +1,10 @@
 class Card:
     def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+        self.suit = suit # Spades, Clubs, Hearts, Diamonds
+        self.value = value # 2-10, Jack, Queen, King, Ace
 
     def show(self):
+        # print to console example: 
+        # 6 of Clubs
         print ("{} of {}".format(self.value, self.suit))
 
