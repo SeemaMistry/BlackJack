b494e4fe122d3136f75784d3e15f068b7c1bc15f
SeemaMistry <seema.m.1996@gmail.com>
2021-07-21 03:28:10 -0400

    Initialize checkTotal(player_index) which checks a player's total. If over 21 it gives a bust statement and changes players_status to false. If ace held in player hand and total over 21, then check total with ace's 2nd value of 1.

--- BlackJack.py
+++ BlackJack.py
@@ -65,6 +65,29 @@ class BlackJack:
     #               check if player went bust (via player.total > 21)
     #               check if player has ace (and bust), then change Player.total by (-= 11) and (+= 1)
     #               if only bust and no ace, then change Player.player_status to false
+    def checkTotal(self, player_index):
+        ace = self.allPlayers[player_index].ace
+        # if player has over 21
+        print("Player '{}' has a total of {}".format(self.allPlayers[player_index].name, self.allPlayers[player_index].total))
+        if self.allPlayers[player_index].total > 21:
+            
+            # check if player holds an ace (double value of 11 and 1)
+            if ace == True:
+                # if there is an ace (and total > 21), has double value of 11 and 1. Add -10 to total
+                self.allPlayers[player_index].total += (-10)
+                # check again if over 21 with new total
+                if self.allPlayers[player_index].total > 21:
+                    # over 21 so bust
+                    self.allPlayers[player_index].player_status = False
+                    print(self.bust(player_index, self.allPlayers[player_index].total))
+            
+            else:
+                # if over 21 but no ace, then change player status to false
+                self.allPlayers[player_index].player_status = False
+                print(self.bust(player_index, self.allPlayers[player_index].total))
+              
+
+        
 
     # pass(player) - player wants no more cards. Change Player.player_status to false
     def ppass(self, player_index):
@@ -73,9 +96,12 @@ class BlackJack:
 
     # call() - check all Player.total to determine winner against the dealer
 
+
     # win(player) - message for winning player. Add point to Player.points
 
     # bust(player) - check if player gone bust. if they do then send message
+    def bust(self, player_index, num):
+        return ("Player '{}' has {}, it's a bust!".format(self.allPlayers[player_index].name, num))
 
     # clearHands() - reset all assests to start a new game. Call Player.deleteHand (including dealer's), deck.build().shuffle(). Increment gameCount
     def clearHands(self):
@@ -141,7 +167,7 @@ game.hit(0)
 game.hit(0)
 game.hit(1)
 game.hit(1)
-game.hit(2)
+#game.hit(2)
 #game.hitDealer()
 # game.hitDealer()
 # game.hitDealer()
@@ -149,6 +175,7 @@ print(game.allPlayers[0].showHand())
 print(game.allPlayers[1].showHand())
 print(game.allPlayers[2].showHand())
 print(game.dealer.dealersHand())
+game.checkTotal(0)
 
 
 
